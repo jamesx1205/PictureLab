155775af54eb32749710d49d087ff193cadf6f09
jamesx1205 <56176490+jamesx1205@users.noreply.github.com>
2020-02-25 15:05:13 -0800

    Countinue

--- pixlab/IntArrayWorker.java
+++ pixlab/IntArrayWorker.java
@@ -11,10 +11,24 @@ public class IntArrayWorker
     matrix = theMatrix;
   }
   
+    public int getCount(int n)
+  {
+    int count = 0;
+    for (int row = 0; row < matrix.length; row++)
+    {
+      for (int col = 0; col < matrix[0].length; col++)
+      {
+        if(matrix[row][col]==n)
+        count++;
+      }
+    }
+    return count;
+  }
   /**
    * Method to return the total 
    * @return the total of the values in the array
    */
+  
   public int getTotal()
   {
     int total = 0;
